b431cd6c0aae49506417899ce90948475ffa4fdc
DraRoVol <107344569+DraRoVol@users.noreply.github.com>
2023-05-12 12:17:21 +0300

    update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,79 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
+import { Component } from 'react';
+import ContactForm from './contactForm/ContactForm';
+import ContactList from './contactList/ContactList';
+import Filter from './filter/Filter';
+
+class App extends Component {
+  state = {
+    contacts: [],
+    filter: '',
+  };
+  // addContact = newContact => {
+  //   this.setState(prevState => ({
+  //     contacts: [...prevState.contacts, newContact],
+  //   }));
+  // };
+  addContact = newContact => {
+  this.setState(prevState => {
+    const updatedContacts = Array.isArray(prevState.contacts) ? [...prevState.contacts, newContact] : [newContact];
+    return { contacts: updatedContacts };
+  });
 };
+  deleteContact = contactId => {
+    this.setState(prevState => ({
+      contacts: prevState.contacts.filter(contact => contact.id !== contactId),
+    }));
+  };
+  handleFilterChange = e => {
+    this.setState({ filter: e.currentTarget.value });
+  };
+  // getFilteredContacts = () => {
+  //   const { contacts, filter } = this.state;
+  //   return contacts.filter(contact =>
+  //     contact.name.toLowerCase().includes(filter.toLowerCase())
+  //   );
+  // };
+  getFilteredContacts() {
+  const { contacts, filter } = this.state;
+  if (contacts !== null) {
+    const filteredContacts = contacts.filter((contact) => {
+      return contact.name.toLowerCase().includes(filter.toLowerCase());
+    });
+    return filteredContacts;
+  }
+  return [];
+}
+
+  componentDidMount() {
+    const parsedContacts = JSON.parse(localStorage.getItem('contacts'));
+    this.setState({ contacts: parsedContacts });
+    if (parsedContacts) {
+      this.setState({contacts: parsedContacts})
+    }
+  }
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.contacts !== prevState.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  }
+  render() {
+    return (
+      <div className="container">
+        <h1>Phonebook</h1>
+        <ContactForm
+          onSubmit={this.addContact}
+          contactList={this.state.contacts}
+        />
+
+        <h2>Contacts</h2>
+        <Filter value={this.state.filter} onChange={this.handleFilterChange} />
+        <ContactList
+          contacts={this.getFilteredContacts()}
+          onDeleteContact={this.deleteContact}
+        />
+      </div>
+    );
+  }
+}
+
+export default App;
